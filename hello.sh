1a718c6ad92042b71d0912037474e8b73d6ad772
nareshchakali <nareshchakali76@gmail.com>
2022-03-13 11:10:20 +0530

    update comments

---
+++ hello.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+echo "Hello everyone"
+echo "GM/GA/GE"
+echo "Today date is"
+date
+echo "welcome to first shell script example"
---
+++ udars.sh
@@ -0,0 +1,7 @@
+a=10
+b=20
+name
+echo "a is "
+echo "b is "
+present dir = $pwd
+echo "The user name is: " $user
